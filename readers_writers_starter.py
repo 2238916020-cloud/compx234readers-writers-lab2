73bc5f04c7fb472b9730abd1b549c17d9fb15827
2238916020-cloud <2238916020@qq.com>
2026-04-01 08:59:56 +0800

    Update readers_writers_starter.py
     Writer post-write release, update and wake all

--- readers_writers_starter.py
+++ readers_writers_starter.py
@@ -107,7 +107,9 @@ class ReadersWritersMonitor:
         """
         with self.condition:
             # TODO: Replace 'pass' with your logic
-            pass
+            self.active_writers -= 1
+            print(f"Write {writer_id} stops writing")
+            self.condition.notify_all()
 
 # Donot Change this
 class Reader(threading.Thread):
